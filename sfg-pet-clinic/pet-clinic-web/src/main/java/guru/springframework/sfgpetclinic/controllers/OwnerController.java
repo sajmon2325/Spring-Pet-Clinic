0b5f13f276b591d85c1f3d1c42a6cf7acd4402e3
sajmon2325 <44872887+sajmon2325@users.noreply.github.com>
2019-03-10 22:33:03 +0100

    Added Owners List to index page
    Modified a method in OwnersController so it retrieves a list of Owners
    Also modified the /owners/index page so it displays list of all owners

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //This is another way of mapping
@@ -9,8 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfOwners(){
+    //Spring will automatically wire a mode bcs we have specified the Mode mode inside ()
+    public String listOfOwners(Model model){
+
+        //Spring MVC will  run this method, this method calls findAll() method from ownerServices
+        //The map that will be created is stored under name "owners" som the View can find this map
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
