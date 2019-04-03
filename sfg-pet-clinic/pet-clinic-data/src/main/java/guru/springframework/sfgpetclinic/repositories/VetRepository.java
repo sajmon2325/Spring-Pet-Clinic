88685c090d1b0bf3f9ee0559a0449a0d120664c9
sajmon2325 <44872887+sajmon2325@users.noreply.github.com>
2019-04-03 23:04:37 +0200

    Implemented JPA Visit Service, added loader data

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.CrudService;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VetRepository extends CrudRepository<Vet, Long> {
