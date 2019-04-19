debf4349efa95bbd5c98e77de16c3521b728bc80
sajmon2325 <44872887+sajmon2325@users.noreply.github.com>
2019-04-19 23:21:37 +0200

    MockitoExtendWith annotation problem

---
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith()
+class OwnerSDJpaServiceTest {
+
+    OwnerRepository ownerRepository;
+    PetRepository petRepository;
+    PetTypeRepository petTypeRepository;
+
+    OwnerSDJpaService service;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findByLastName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
